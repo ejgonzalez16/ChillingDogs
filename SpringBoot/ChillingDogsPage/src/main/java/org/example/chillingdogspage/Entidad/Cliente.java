480ea265f204656c8afa47dd2c5c2582b5db64b3
Édgar Julián González Sierra <edgarjuliangonzalezsierra167@gmail.com>
2024-08-18 15:27:47 -0500

    Cambios entidad Cliente

---
+++ SpringBoot/ChillingDogsPage/src/main/java/org/example/chillingdogspage/Entidad/Cliente.java
@@ -0,0 +1,66 @@
+package org.example.chillingdogspage.Entidad;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.List;
+
+public class Cliente {
+    private String cedula;
+    private String nombre;
+    private String correo;
+    private String celular;
+    private List<Mascota> mascotas;
+
+    public Cliente(String cedula, String nombre, String correo, String celular, List<Mascota> mascotas) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.celular = celular;
+        this.mascotas = mascotas;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public List<Mascota> getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(List<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    public List<Mascota> mostrarMascotasTratamiento() throws ExecutionControl.NotImplementedException {
+        // TODO
+        throw new ExecutionControl.NotImplementedException("No implementado :D");
+    }
+}
