3875cfec8b1d7cc502ee3a318b2347bba7d0016b
Édgar Julián González Sierra <edgarjuliangonzalezsierra167@gmail.com>
2024-08-18 20:06:05 -0500

    pantalla login front

---
+++ SpringBoot/ChillingDogsPage/src/main/java/org/example/chillingdogspage/Controlador/LoginController.java
@@ -0,0 +1,18 @@
+package org.example.chillingdogspage.Controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login/")
+public class LoginController {
+
+    //http://localhost:8099/login/
+    @GetMapping()
+    public String login(){
+        return "login";
+    }
+}
