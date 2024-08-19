0dde001dddab491fc6c16e79ba143b0ae4d0e45d
silvag-daniels <silvagdaniels1309@gmail.com>
2024-08-18 19:33:36 -0500

    Coomit parcial 2

--- SpringBoot/ChillingDogsPage/src/main/java/org/example/chillingdogspage/Repositorio/ClienteRepository.java
+++ SpringBoot/ChillingDogsPage/src/main/java/org/example/chillingdogspage/Repositorio/ClienteRepository.java
@@ -15,26 +15,17 @@ import java.util.Map;
 public class ClienteRepository {
     private Map<Integer, Cliente> data = new HashMap<>();
 
-    @Autowired
-    MascotaRepository mascotaRepository;
-
     public ClienteRepository(){
         List<Mascota> mascotas = new ArrayList<>();
-        mascotas.add(mascotaRepository.findById(1));
-        mascotas.add(mascotaRepository.findById(2));
         data.put(1, new Cliente("1234567891", "Daniel Silva", "silva@silva.com", "3155784455", mascotas));
-        mascotas.clear();
-        mascotas.add(mascotaRepository.findById(3));
         data.put(2, new Cliente("3245789023", "Kurosaki Carlos", "carlos@carlos.com", "3234567789", mascotas));
-        mascotas.clear();
-        mascotas.add(mascotaRepository.findById(4));
         data.put(3, new Cliente("5234567890", "Julius Novachrono", "edgar@edgar.com", "3195678341", mascotas));
     }
 
     public Cliente findByCedula(String cedula){
         for (Map.Entry<Integer, Cliente> entry : data.entrySet()) {
             if (entry.getValue().getCedula().equals(cedula)) {
-                return entry.getValue(); // Retorna el cliente encontrado
+                return entry.getValue();
             }
         }
         return null;
